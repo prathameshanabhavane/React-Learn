84e0435a71e534530d22a4801f61bb51d36cb8d7
Prathameshanabhavane <pratham291994@gmail.com>
2019-07-06 16:55:00 +0530

    let var and const understand

---
+++ app/src/index.js
@@ -0,0 +1,28 @@
+
+// car, ;et and const keyword
+
+// var -> fucntion
+// let -> block
+//const -> block
+
+// function sayHello() {
+//     for(var i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+
+
+// function sayHello() {
+//     for(let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+
+// sayHello();
+
+// const x = 1;
+// x = 2;
+
+
