f2a21f7a312627ddf3cf061baa081ad4a1a87ef6
Prathameshanabhavane <pratham291994@gmail.com>
2019-07-14 16:33:36 +0530

    handling events

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,24 +5,32 @@ class Counter extends Component {
        count : 0,
        tages : ['tag1', 'tag2', 'tag3']
     };
-    
-    renderTags() {
-        if  (this.state.tages.length === 0) return <p>Pragraphas has no tgas! </p>
 
-        return <ul> {this.state.tages.map( tag => <li key={tag}> {tag} </li> ) } </ul>
+    handleIncreament() {
+        console.log('Handling Increament', this);
     }
-
+    
     render() { 
         return ( 
             <div>
-                {/* {this.state.tages.length === 0 && 'Please create new tag'} //another option to write condition */}
-                <ul>
-                   {this.renderTags()}
-                </ul>
+                <span className={this.getBadgeClasses()}> {this.fromatCount()} </span> 
+                <button onClick = {this.handleIncreament} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
 
+    getBadgeClasses() {
+        let classes = "dadge m-3 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    fromatCount() {
+        const { count } = this.state; // using object destructuring
+        // return this.state.count === 0 ? 'Zero' : this.state.count
+            return count === 0 ? 'Zero' : count;
+        // return count === 0 ? <h1>Zero </h1> : count;
+    }
 }
  
 export default Counter;
