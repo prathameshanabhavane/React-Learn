b7103aafc4fc6bd6c9a1f176ada3d2486069d230
Prathameshanabhavane <pratham291994@gmail.com>
2019-07-20 12:00:49 +0530

    composing compnents and passing data to componenets

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-       count : 0,
-       tages : ['tag1', 'tag2', 'tag3']
+       value : this.props.value
     };
 
     // constructor() {
@@ -15,7 +14,7 @@ class Counter extends Component {
 
     handleIncreament = product => {
         console.log(product);
-        this.setState({ count: this.state.count + 1});
+        this.setState({ value: this.state.value + 1});
     }
 
     // handleIncreament() {
@@ -23,6 +22,7 @@ class Counter extends Component {
     // }
     
     render() { 
+        // console.log("props", this.props)
         return ( 
             <div>
                 <span className={this.getBadgeClasses()}> {this.fromatCount()} </span> 
@@ -33,14 +33,14 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-3 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     fromatCount() {
-        const { count } = this.state; // using object destructuring
-        // return this.state.count === 0 ? 'Zero' : this.state.count
-            return count === 0 ? 'Zero' : count;
+        const { value } = this.state; // using object destructuring
+        // return this.state.value === 0 ? 'Zero' : this.state.value
+            return value === 0 ? 'Zero' : value;
         // return count === 0 ? <h1>Zero </h1> : count;
     }
 }
