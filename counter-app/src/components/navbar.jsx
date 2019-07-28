08c331c64f54190390f7c0594f786befa8ee9fac
Prathameshanabhavane <pratham291994@gmail.com>
2019-07-28 16:43:34 +0530

    stateless function compenent

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
+// Stateless Functional Compenent
 
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar <span class="badge badge-pill badge-info">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-
-            );
-    }
+const NavBar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar {" "} <span class="badge badge-pill badge-info">{props.totalCounters}</span>
+            </a>
+        </nav>
+    );
 }
- 
+
 export default NavBar;
