81ae1c3782d933eb9fd984f6a41a65f1f2d22da4
Prathameshanabhavane <pratham291994@gmail.com>
2019-08-11 23:17:17 +0530

    filtering component interface

--- vidly/src/component/Movies.js
+++ vidly/src/component/Movies.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Like from './common/like';
+import { ListGroup } from './common/listGroup';
 import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService.js';
 import { paginate } from '../utils/pajinate';
+import { getGenres } from '../services/fakeGenreService';
 
 
 class Movies extends Component {
     state = { 
         movies : getMovies(),
+        genres : [],
         currentPage : 1,
         pageSize : 4
      }
+
+    componentDidMount() {
+      this.setState({ 
+        movie : getMovies(),
+        genres : getGenres()
+      });
+    }
 
     handleDelete = (movie) => {
          console.log(movie);
@@ -34,6 +44,9 @@ class Movies extends Component {
       this.setState({ currentPage : page });
     }
 
+    handeleGenreSelect = genre => {
+      console.log(genre);
+    }
 
     render() { 
         const  { length : count } = this.state.movies;
@@ -45,38 +58,43 @@ class Movies extends Component {
         const movies = paginate(allMoveis, currentPage, pageSize);
 
         return (  
-        <React.Fragment>
-        <p className="pt-3">Showing {count} movies in database.  </p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-              <th scope="col">Like</th>
-              <th scope="col">Event</th>
-            </tr>
-          </thead>
-          <tbody>
-          { movies.map( movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)} /></td>
-              <td><button type="button" onClick={() => this.handleDelete(movie)} className="btn btn-danger">Delete</button></td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
-        <Pagination 
-          itemsCount={count} 
-          pageSize={pageSize} 
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange} />
-        </React.Fragment>
+        <div className="row">
+          <div class="col-12 col-md-2">
+              <ListGroup items={this.state.genres} onItemsSelect={this.handeleGenreSelect} />
+          </div>
+          <div class="col-12 col-md-10">
+            <p className="pt-3">Showing {count} movies in database.  </p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Stock</th>
+                  <th scope="col">Rate</th>
+                  <th scope="col">Like</th>
+                  <th scope="col">Event</th>
+                </tr>
+              </thead>
+              <tbody>
+              { movies.map( movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)} /></td>
+                  <td><button type="button" onClick={() => this.handleDelete(movie)} className="btn btn-danger">Delete</button></td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
+            <Pagination 
+              itemsCount={count} 
+              pageSize={pageSize} 
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange} />
+          </div>
+        </div>
         );
     }
 }
