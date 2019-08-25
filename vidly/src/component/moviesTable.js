efd242b17a6d4ce63088e1330000f3d44e60d889
Prathameshanabhavane <pratham291994@gmail.com>
2019-08-25 19:54:19 +0530

    Sorting : extracting a method

--- vidly/src/component/moviesTable.js
+++ vidly/src/component/moviesTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import Table from './common/table';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+
 
 class MoviesTable extends Component {
     
@@ -23,14 +23,7 @@ class MoviesTable extends Component {
     render() { 
         const { movies, onSort, sortColumn } = this.props;
         return ( 
-            <table className="table">
-                <TableHeader 
-                    columns={this.columns} 
-                    sortColumn={sortColumn} 
-                    onSort={onSort} 
-                />
-                <TableBody data={movies} columns={this.columns}/>
-            </table>
+            <Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort} />
          );
     }
 }
