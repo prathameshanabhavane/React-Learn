7cb03c03ddae083e47e37dd0b14b546d3193b17c
Prathameshanabhavane <pratham291994@gmail.com>
2019-08-26 02:02:32 +0530

    programatic navigation

--- router-app/src/components/productDetails.jsx
+++ router-app/src/components/productDetails.jsx
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    // this.props.history.push('/products');
+    this.props.history.replace('/products');
+
   };
 
   render() {
