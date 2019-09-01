b624ad733eceb6c1288edde25229c47fd8968d3a
Prathameshanabhavane <pratham291994@gmail.com>
2019-09-01 11:50:12 +0530

    adding react router

---
+++ vidly/src/component/movieForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MovieForm extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+        );
+    }
+}
+ 
+export default MovieForm;
