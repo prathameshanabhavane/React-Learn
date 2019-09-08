4375692e934a412d81c88b01fa20939d847fc969
Prathamesh Anabhavane <pratham291994@gmail.com>
2019-09-08 19:20:08 +0530

    Handling multiple inputs

--- vidly/src/component/login.jsx
+++ vidly/src/component/login.jsx
@@ -23,24 +23,25 @@ class Login extends Component {
 		console.log('Submitted');
 	}
 
-	handleChange = e => {
+	handleChange = ({ currentTarget: input }) => {
 		const account = { ...this.state.account };
-		account.username = e.currentTarget.value;
+		account[input.name] = input.value;
 		this.setState({ account });
 	}
 
 	render(){
+		const { account } = this.state;
 		return(
 			<div className="row align-item-center">
 				<div className="col-12 col-lg-4 mx-auto">
 					<form onSubmit={this.handleSubmit}>
 					  <div className="form-group">
 						<label htmlFor="username">Username</label>
-						<input value={this.state.account.username} onChange={this.handleChange} autoFocus ref={this.username} type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter email" />
+						<input value={account.username} onChange={this.handleChange} name="username" autoFocus ref={this.username} type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter email" />
 					  </div>
 					  <div className="form-group">
 						<label htmlFor="password">Password</label>
-						<input type="password" className="form-control" id="password" placeholder="Password" />
+						<input value={account.password} onChange={this.handleChange} name="password" type="password" className="form-control" id="password" placeholder="Password" />
 					  </div>
 					  <button type="submit" className="btn btn-primary">Submit</button>
 					</form>
