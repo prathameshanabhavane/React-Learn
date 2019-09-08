48edd85833e6b142c7a102e9deb23ff678a2e31b
Prathamesh Anabhavane <pratham291994@gmail.com>
2019-09-08 19:11:44 +0530

    controlled elements

--- vidly/src/component/login.jsx
+++ vidly/src/component/login.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class Login extends Component {
 
+	state = {
+		account : {
+			username : ' ',
+			password : ' '
+		}
+	}
+
 	username = React.createRef();
 
 	// componentDidMount() {
@@ -16,6 +23,12 @@ class Login extends Component {
 		console.log('Submitted');
 	}
 
+	handleChange = e => {
+		const account = { ...this.state.account };
+		account.username = e.currentTarget.value;
+		this.setState({ account });
+	}
+
 	render(){
 		return(
 			<div className="row align-item-center">
@@ -23,7 +36,7 @@ class Login extends Component {
 					<form onSubmit={this.handleSubmit}>
 					  <div className="form-group">
 						<label htmlFor="username">Username</label>
-						<input autoFocus ref={this.username} type="email" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter email" />
+						<input value={this.state.account.username} onChange={this.handleChange} autoFocus ref={this.username} type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter email" />
 					  </div>
 					  <div className="form-group">
 						<label htmlFor="password">Password</label>
