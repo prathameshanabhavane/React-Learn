8dd06f416e84846abb946023d29bc3d301bc045c
Prathameshanabhavane <pratham291994@gmail.com>
2019-10-26 20:06:50 +0530

    validating a form using Joi

--- vidly/src/component/login.jsx
+++ vidly/src/component/login.jsx
@@ -5,8 +5,8 @@ class Login extends Component {
 
 	state = {
 		account : {
-			username : ' ',
-			password : ' '
+			username : '',
+			password : ''
 		},
 		errors : {}
 	};
@@ -18,26 +18,20 @@ class Login extends Component {
 	// }
 
 	schema = {
-		username: Joi.string().required(),
-		password: Joi.string().required()
+		username: Joi.string().required().label('Username'),
+		password: Joi.string().required().label('Password')
 	};
 
 	validate = () => {
-
-		const result = Joi.validate(this.state.account, this.schema, {
-			abortEarly : false
-		});
+		const options = { abortEarly : false };
+		const result = Joi.validate(this.state.account, this.schema, options);
 		console.log(result);
+		if(!result.error) return null;
 
 		const errors = {};
-
-		const { account } = this.state;
-		if(account.username.trim() === '')
-			errors.username = 'Username is required.';
-		if(account.password.trim() === '')
-			errors.password = 'Password is required.';
-
-		return Object.keys(errors).length === 0 ? null : errors;
+		for(let item of result.error.details)
+			errors[item.path[0]] = item.message;
+			return errors;
 	}
 
 	handleSubmit = e => {
