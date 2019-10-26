17ebf9e276084ecd89c56a52e2ccd613c3833be4
Prathameshanabhavane <pratham291994@gmail.com>
2019-10-26 20:08:26 +0530

    Validating a field using Joi

--- vidly/src/component/login.jsx
+++ vidly/src/component/login.jsx
@@ -47,14 +47,10 @@ class Login extends Component {
 	}
 
 	validateProperty = ({ name, value }) => {
-		if (name === 'username') {
-			if(value.trim() === '') return "Username is required.";
-			// ...
-		}
-		if (name === 'password') {
-			if(value.trim() === '') return "Password is required.";
-			// ...
-		}
+		const obj = { [name]: value };
+		const schema = { [name]: this.schema[name] };
+		const {error} = Joi.validate(obj, schema, { abortEarly : false });
+		return error ? error.details[0].message : null;
 	}
 
 	handleChange = ({ currentTarget: input }) => {
