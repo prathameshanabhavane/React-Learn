c1257103c2fdda318683ebf096c9cd9d910651c2
Prathameshanabhavane <pratham291994@gmail.com>
2019-10-27 12:23:54 +0530

    extracting a reusable form

--- vidly/src/component/login.jsx
+++ vidly/src/component/login.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
+import Form from './common/form';
 import Input from './common/input';
-class Login extends Component {
+
+class Login extends Form {
 
 	state = {
-		account : {
+		data : {
 			username : '',
 			password : ''
 		},
@@ -22,65 +24,28 @@ class Login extends Component {
 		password: Joi.string().required().label('Password')
 	};
 
-	validate = () => {
-		const options = { abortEarly : false };
-		const result = Joi.validate(this.state.account, this.schema, options);
-		console.log(result);
-		if(!result.error) return null;
 
-		const errors = {};
-		for(let item of result.error.details)
-			errors[item.path[0]] = item.message;
-			return errors;
-	}
-
-	handleSubmit = e => {
-		e.preventDefault();
-
-		const errors = this.validate();
-		// console.log(errors);
-		this.setState({ errors : errors || {} });
-		if(errors) return;
-
+    doSubmit = () => {
 		// call the server
 		console.log('Submitted');
 	}
 
-	validateProperty = ({ name, value }) => {
-		const obj = { [name]: value };
-		const schema = { [name]: this.schema[name] };
-		const {error} = Joi.validate(obj, schema, { abortEarly : false });
-		return error ? error.details[0].message : null;
-	}
-
-	handleChange = ({ currentTarget: input }) => {
-		const  errors = { ...this.state.errors }
-		const errorMessage = this.validateProperty(input);
-		if(errorMessage) errors[input.name] = errorMessage;
-		else delete errors[input.name];
-
-
-		const account = { ...this.state.account };
-		account[input.name] = input.value;
-		this.setState({ account, errors });
-	}
-
 	render(){
-		const { account, errors } = this.state;
+		const { data, errors } = this.state;
 		return(
 			<div className="row align-item-center">
 				<div className="col-12 col-lg-4 mx-auto">
 					<form onSubmit={this.handleSubmit}>
 					  	<Input 
 						  name="username" 
-						  value={account.username} 
+						  value={data.username} 
 						  label="Username" 
 						  onChange={this.handleChange} 
 						  error={errors.username}
 						/>
 						<Input 
 						  name="password" 
-						  value={account.password} 
+						  value={data.password} 
 						  label="Password" 
 						  onChange={this.handleChange} 
 						  error={errors.password}
